3a0f7ff3537dd6df88f9ffaf40992dd3959d43bb
izaz4141 <izaz4141@users.noreply.github.com>
2022-11-03 14:10:49 +0000

    deploy: d3d460c020f6f8ec044d7e188597fb74be8abc14

--- assets/AboutPage.34d27154.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index.802e67aa.js";const t={};function n(r,s){return c(),o("div",null,"This is AboutPage")}const _=e(t,[["render",n]]);export{_ as default};
--- assets/HomePage.1617a5ea.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index.802e67aa.js";const n={};function r(s,t){return c(),o("div",null,"This is HomePage")}const _=e(n,[["render",r]]);export{_ as default};
