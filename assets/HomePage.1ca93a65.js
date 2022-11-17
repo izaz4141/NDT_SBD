05b7c7cf58f4f856f3ecee225e45c1ebbb54d5f1
izaz4141 <izaz4141@users.noreply.github.com>
2022-11-17 14:00:27 +0000

    deploy: 902322777dca6d761983237a315746e6dbf847ca

---
+++ assets/HomePage.1ca93a65.js
@@ -0,0 +1,1 @@
+import{i as t,o as a,c,a as e,t as n,u as o}from"./index.092fe7e2.js";const i={class:"text-center"},r=e("div",null,"This is HomePage",-1),m={__name:"HomePage",setup(_){const s=t("screenSize");return(l,u)=>(a(),c("div",i,[r,e("div",null,n(o(s)),1)]))}};export{m as default};
