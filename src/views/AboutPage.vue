de3eac5390ef819a317ac14d90f50e9e9c065cf1
SatrioGilang <satrioisma@gmail.com>
2022-11-30 14:25:35 +0700

    AboutPage

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>This is AboutPage</div>
+  <div>
+  <h1>
+  Tentang Perusahaan</h1>
+      <h2>Profil Perusahaan</h2>
+          <p>Perusahaan ini bergerak dalam bidang jasa Uji 
+              Tak Rusak/Non-Destructive Test (NDT) pada bangunan 
+              dan infrastruktur. Perusahaan ini menawarkan 
+              jasa NDT berpresisi tinggi dengan sinar gamma dan sinar-X.</p>
+      <h2>Visi Misi dan Tujuan</h2>
+          <h3>Visi</h3>
+              <p>Menjadi perusahaan yang profesional dan handal dalam jasa inspeksi dan
+                  NDT service dengan teknologi nuklir.</p>
+          <h3>Misi</h3>
+          <ol>
+              <li>Menggunakan peralatan NDT berteknologi nuklir dengan mutu tinggi</li>
+              <li>Memperluas penerapan dan lapangan kerja dan teknik NDT</li>
+              <li>Menyediakan layanan NDT dengan harga yang bersaing</li>
+          </ol>
+          <h3>Tujuan</h3>
+          <ol>
+              <li></li>
+              <li></li>
+              <li></li>
+          </ol>
+  </div>
 </template>
 
 <script setup></script>
