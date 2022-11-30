391371d6637ee9e5d2f4b7bfab1334b729432126
SatrioGilang <SatrioGilang@users.noreply.github.com>
2022-11-30 08:33:03 +0000

    deploy: 2224f1ef674dbd32160e4485c6c3457ca1fdc364

--- assets/DaftarKerja.10c7e062.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c}from"./index.139a053b.js";const r={};function s(t,n){return a(),c("div",null,"This is Task List Page")}const _=e(r,[["render",s]]);export{_ as default};
--- assets/DevPage.043eecff.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index.139a053b.js";const o={};function r(s,t){return c(),n("div",null,"This is Dev Page")}const _=e(o,[["render",r]]);export{_ as default};
