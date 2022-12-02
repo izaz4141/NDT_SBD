8e1ca47133c4e281c61cfcad1bea12ee96f86062
SatrioGilang <satrioisma@gmail.com>
2022-12-02 18:33:29 +0700

    rapihin code

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class='mx-4 my-3'>
-    <h1 class="text-3xl text-center font-bold">GamMAX NDT</h1><br/>
+  <div class="mx-4 my-3">
+    <h1 class="text-3xl text-center font-bold">GamMAX NDT</h1>
+    <br />
     <h2 class="text-2xl">Solusi Kebutuhan NDT Anda</h2>
-    <img class="inline" src="../assets/gamma_camera.png" width="400px" height="400px">
-    <img class="inline" src="../assets/x-ray_set.png" width="400px" height="400px">
+    <img
+      class="inline"
+      src="../assets/gamma_camera.png"
+      width="400px"
+      height="400px"
+    />
+    <img
+      class="inline"
+      src="../assets/x-ray_set.png"
+      width="400px"
+      height="400px"
+    />
     <p class="text-center">
-      GamMAX NDT adalah perusahaan yang bergerak di bidang jasa Uji Tak Rusak/Non-Destructive Test (NDT) pada bangunan dan infrastruktur. Perusahaan ini menawarkan jasa NDT berpresisi tinggi dengan teknologi nuklir, yaitu sinar gamma dan sinar-X.
-      <br/>
-      Dengan teknisi ahli dari GamMAX, kami selalu mengusahakan pelayanan terbaik dengan harga yang bersaing bagi semua pelanggan. Kami sangat mengutamakan aspek proteksi radiasi, keselamatan kerja, dan pelayanan yang berkualitas.
+      GamMAX NDT adalah perusahaan yang bergerak di bidang jasa Uji Tak
+      Rusak/Non-Destructive Test (NDT) pada bangunan dan infrastruktur.
+      Perusahaan ini menawarkan jasa NDT berpresisi tinggi dengan teknologi
+      nuklir, yaitu sinar gamma dan sinar-X.
+      <br />
+      Dengan teknisi ahli dari GamMAX, kami selalu mengusahakan pelayanan
+      terbaik dengan harga yang bersaing bagi semua pelanggan. Kami sangat
+      mengutamakan aspek proteksi radiasi, keselamatan kerja, dan pelayanan
+      yang berkualitas.
     </p>
     <br />
     <h2 class="text-2xl">Fitur</h2>
@@ -18,7 +35,8 @@
       <li>Peralatan berkualitas</li>
       <li>Cepat dan tepat</li>
       <li>Teknisi yang handal dan professional</li>
-    </ol> <br/>
+    </ol>
+    <br />
     <h2 class="text-2xl">Kontak Kami:</h2>
   </div>
 </template>
