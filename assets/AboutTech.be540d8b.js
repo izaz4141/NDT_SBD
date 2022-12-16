9f7ba49fdf4f85d73e179025a916918ff0502462
SatrioGilang <SatrioGilang@users.noreply.github.com>
2022-12-16 08:24:41 +0000

    deploy: 5653bf37c394b6ac4b83409de2433ea1e568f781

--- assets/AboutTech.be540d8b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index.8f0a7977.js";const t={};function n(r,s){return c(),o("div",null,"This is About Tech Page")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/x-ray_set.866f0058.js
@@ -0,0 +1,1 @@
+const s="/NDT_SBD/assets/gamma_camera.f035bec3.png",a="/NDT_SBD/assets/x-ray_set.bf374b3e.png";export{s as _,a};
